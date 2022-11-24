b4f3446e280e9ee8721bcfe70b551407ad029fee
abdusami <3338231042@qq.com>
2022-11-24 16:39:26 +0800

    first commit

---
+++ src/main/java/com/asmi/yilimusic/controller/DefaultController.java
@@ -0,0 +1,19 @@
+package com.asmi.yilimusic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+
+
+    @GetMapping
+    public String sayHello(){
+        return "Welcome Yili-Music!";
+    }
+
+
+}
